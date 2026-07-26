48d35c0bc11be6791ed47178c92aecaa81d0e90c
Anurag Pal <anuragpal9002@gmail.com>
2026-07-26 15:29:57 +0530

    Time: 58 ms (13.54%) | Memory: 185.3 MB (7.86%) - LeetSync

---
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long maxSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Long> map = new HashMap<>();
+        map.put(0, 0L);
+        long prefix = 0;
+        long ans = Long.MIN_VALUE;
+        for(int i = 0; i<nums.length; i++){
+            prefix+= nums[i];
+            int rem = (i+1)%k;
+            if(map.containsKey(rem)){
+                ans = Math.max(ans, prefix - map.get(rem));
+            }
+            map.put(rem, Math.min(map.getOrDefault(rem, Long.MAX_VALUE), prefix));
+        }
+        return ans;
+    }
+}
